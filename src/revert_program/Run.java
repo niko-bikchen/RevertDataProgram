39e7f2a48025f77d1e192318167424c7d9e864a2
Nikolay Bikchentaev <ironhead749@gmail.com>
2019-03-30 12:33:30 +0200

    This class runs the program

---
+++ src/revert_program/Run.java
@@ -0,0 +1,19 @@
+package revert_program;
+
+/**
+ * Run class
+ * 
+ * <P>
+ * Runs the program.
+ * 
+ * @author Nikolay Bikchentaev
+ * @since 30.03.2019
+ * @version 1.0
+ */
+
+public class Run {
+
+	public static void main(String[] args) {
+		MainWindow window = new MainWindow();
+	}
+}
